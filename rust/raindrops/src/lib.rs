8c8b0b100daea2265990448c4af9ad40f89aeab6
Wudong Liu <wudong.liu@gmail.com>
2022-01-10 13:54:52 +0000

    add raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let divisor:[u32; 3] =[3,5,7];
+    let str:[&str; 3] =["Pling","Plang","Plong"];
+    
+    let strs: Vec<&str> = divisor.iter().zip(str)
+           .filter_map(|(d, s)| if n % d == 0 {Some(s)} else {None})
+           .collect();
+    match strs.len() {
+        0 => n.to_string(),
+        _ => strs.join(""),
+    } 
 }
