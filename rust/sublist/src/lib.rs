b7997f963ed82728d12c1e48ff3f5e830349db12
Wudong Liu <wudong.liu@gmail.com>
2022-01-16 18:30:52 +0000

    generic fn

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -43,29 +43,29 @@ pub fn check_contain<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> boo
         return true;
     }
 
-    let mut iter1 = _first_list.iter().skip_while(|&x| *x != _second_list[0]);
-    let mut iter2 = _second_list.iter();
+    let iter1 = _first_list.iter().skip_while(|&x| *x != _second_list[0]);
+    let iter2 = _second_list.iter();
 
-    let c1 = loop {
-        let (e1, e2) = (iter1.next(), iter2.next());
-        let keep_going = match (e1, e2) {
-            (Some(v1), Some(v2)) if *v1 == *v2 => true,
-            _ => false,
-        };
-        if keep_going {
-            continue;
-        } else {
-            break e2.is_none();
-        }
-    };
+    let c1 = loop_check(iter1, iter2);
 
-    let mut iter1 = _first_list
+    let iter1 = _first_list
         .iter()
         .rev()
         .skip_while(|&x| *x != _second_list[_second_list.len() - 1]);
-    let mut iter2 = _second_list.iter().rev();
+    let iter2 = _second_list.iter().rev();
 
-    let c2 = loop {
+    let c2 = loop_check(iter1, iter2);
+
+    return c1 || c2;
+}
+
+fn loop_check<'a, T>(iter1: impl Iterator<Item = &'a T>, iter2: impl Iterator<Item = &'a T>) -> bool
+where
+    T: 'a + PartialEq,
+{
+    let mut iter1 = iter1;
+    let mut iter2 = iter2;
+    loop {
         let (e1, e2) = (iter1.next(), iter2.next());
         let keep_going = match (e1, e2) {
             (Some(v1), Some(v2)) if *v1 == *v2 => true,
@@ -74,9 +74,7 @@ pub fn check_contain<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> boo
         if keep_going {
             continue;
         } else {
-            break e2.is_none();
+            return e2.is_none();
         }
-    };
-
-    return c1 || c2;
+    }
 }
